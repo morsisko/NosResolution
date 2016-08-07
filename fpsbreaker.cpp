9ce8c1559e193eef2cec70060483e472c6db9a3b
morsisko <morsisko@gmail.com>
2016-08-07 19:17:38 +0200

    code formating

--- fpsbreaker.cpp
+++ fpsbreaker.cpp
@@ -2,7 +2,10 @@
 
 FpsBreaker::FpsBreaker()
 {
-    fpsLimit = *(int**)(scanner.findPattern((BYTE*)(0x400000), 0x200000, (BYTE*)("\xA3\x00\x00\x00\x00\xEB\x0A"), (char*)("x????xx")) + 1);
+    fpsLimit = *(int**)(scanner.findPattern((BYTE*)(0x400000),
+                                            0x200000,
+                                            (BYTE*)("\xA3\x00\x00\x00\x00\xEB\x0A"),
+                                            (char*)("x????xx")) + 1);
 }
 
 void FpsBreaker::setFpsLimit(int value)
--- resolutionbreaker.cpp
+++ resolutionbreaker.cpp
@@ -2,9 +2,20 @@
 
 ResolutionBreaker::ResolutionBreaker()
 {
-    callAddy = (DWORD)(scanner.findPattern((BYTE*)(0x400000), 0x200000, (BYTE*)("\x55\x8B\xEC\x51\x53\x56\x57\x8B\xF9\x8B\xF2\x8B\xD8\xC6\x45\xFF\x01\x8B\x45\x08"), (char*)("xxxxxxxxxxxxxxxxxxxx")));
-    formClose = *(DWORD*)(scanner.findPattern((BYTE*)(0x550000), 0x200000, (BYTE*)("\xA1\x00\x00\x00\x00\x8B\x80\x00\x00\x00\x00\x8B\x50\x5C"), (char*)("x????xx????xxx")) + 1);
-    windowBase = *(DWORD*)(scanner.findPattern((BYTE*)(0x550000), 0x200000, (BYTE*)("\x8B\x1D\x00\x00\x00\x00\x8A\x1B\xA1\x00\x00\x00\x00"), (char*)("xx????xxx????")) + 2);
+    callAddy = (DWORD)(scanner.findPattern((BYTE*)(0x400000),
+                                           0x200000,
+                                           (BYTE*)("\x55\x8B\xEC\x51\x53\x56\x57\x8B\xF9\x8B\xF2\x8B\xD8\xC6\x45\xFF\x01\x8B\x45\x08"),
+                                           (char*)("xxxxxxxxxxxxxxxxxxxx")));
+
+    formClose = *(DWORD*)(scanner.findPattern((BYTE*)(0x550000),
+                                              0x200000,
+                                              (BYTE*)("\xA1\x00\x00\x00\x00\x8B\x80\x00\x00\x00\x00\x8B\x50\x5C"),
+                                              (char*)("x????xx????xxx")) + 1);
+
+    windowBase = *(DWORD*)(scanner.findPattern((BYTE*)(0x550000),
+                                               0x200000,
+                                               (BYTE*)("\x8B\x1D\x00\x00\x00\x00\x8A\x1B\xA1\x00\x00\x00\x00"),
+                                               (char*)("xx????xxx????")) + 2);
 }
 
 void ResolutionBreaker::changeResolution(int x, int y)
